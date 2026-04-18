c391c53b8cdfc3aca58b6e695a3d49029168e79d
AustinBorchardt2027 <computerbug@duck.com>
2026-04-18 17:46:05 -0600

    Update script.js

--- script.js
+++ script.js
@@ -8,7 +8,7 @@
 // Sheet published as CSV
 const SHEET_CSV_URL = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vRRk-WuFbb7q-_ZNbCjC6AaeV5yR6cGDuVCBJp0-wQI3zRQmdSaw87uzsUwI3dFgXTvsO_qBs6ach1C/pub?output=csv';
 // ↓↓ PASTE YOUR APPS SCRIPT /exec URL HERE ↓↓
-const DRIVE_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbw6v5I5NagnyMJFNF6jnsrOS2kdvZo5EUO-_PQBNBwSgOmr3NVtJe_-Sqh_kuvgVa4k/exec';
+const DRIVE_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbw6v5I5NagnyMJFNF6jnsrOS2kdvZo5EUO-_PQBNBwSgOmr3NVtJe_-Sqh_kuvgVa4k/exechttps://script.google.com/macros/s/AKfycbw6v5I5NagnyMJFNF6jnsrOS2kdvZo5EUO-_PQBNBwSgOmr3NVtJe_-Sqh_kuvgVa4k/exec';
 
 // ─── DEMO DATA ────────────────────────────────────────────────
 const DEMO_MOVIES = [
